2aa7aa3eb758e29e04feac3caf7cc3d73da405a8
meamme <doshiboy01@gmail.com>
2015-04-06 13:25:25 -0600

    added suff\

---
+++ ext2/injJS.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundImage = "url('http://i.imgur.com/LmuELPV.jpg')";
+document.getElementById("blueBarNAXAnchor").style.backgroundImage = "url('http://i.imgur.com/LmuELPV.jpg')";
+document.getElementById("blueBarDOMInspector").style.backgroundImage = "url('http://i.imgur.com/LmuELPV.jpg')";
+document.getElementById("pagelet_bluebar").style.backgroundImage = "url('http://i.imgur.com/LmuELPV.jpg')";
+document.getElementById("content").style.background = "#00000000";
+
+document.getElementById("u_ps_0_0_0").style.background = "#000000";
+document.getElementById("topnews_main_stream_408239535924329").style.background = "#000000";
+document.getElementById("stream_pagelet").style.background = "#00000000";
+document.getElementById("rightCol").style.background = "#00000000";
+document.getElementById("contentCol").style.background = "#00000000";
+document.getElementById("mainContainer").style.background = "#00000000";
+document.getElementById("contentcontentArea").style.background = "#00000000";
